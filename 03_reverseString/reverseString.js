f502a997f9594980549a0cf4c1268c975c1b79e1
farzaan7 <farzanfayaz07@gmail.com>
2024-09-11 14:50:20 +0530

    js

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,8 +1,9 @@
-const reverseString = function() {
-    let string = "hello";
-    return reverseString(string);
-
-};
-
-// Do not edit below this line
-module.exports = reverseString;
+const reverseString = function(str = "") {
+    if (str === "") {
+      return "";
+    }
+    return reverseString(str.slice(1)) + str[0];
+  };
+  
+  // Do not edit below this line
+  module.exports = reverseString;
