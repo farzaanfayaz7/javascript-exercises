b050dd1e63c9640d783c769896e5c322b08ead7b
farzaan7 <farzanfayaz07@gmail.com>
2024-09-11 14:24:06 +0530

    add js

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,8 +1,8 @@
-const repeatString = function(string, num) {
-for(var i=0; i<=num; i++) {
-    console.log(string);
-}
+const repeatString = function (string, num) {
+    for (var i = 0; i < num; i++) {
+        console.log(string);
+    }
 };
-repeatString();
+repeatString("hey", 3);
 // Do not edit below this line
 module.exports = repeatString;
